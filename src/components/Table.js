43379e4bc6aec5d9c37b8a70c69bfa5992770170
Thomas-delattre <delattre.thomas@cegetel.net>
2024-03-04 15:28:48 +0100

    tablejs

--- src/components/Table.js
+++ src/components/Table.js
@@ -169,8 +169,8 @@ const Table = ({ coinsData }) => {
                 return (
                   a.ath_change_percentage - b.ath_change_percentage
                   );
-                  default:
-                    null;
+                  // default:
+                  //   null;
                 }
               })
           .map((coin, index) => (
